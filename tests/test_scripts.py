9a51503139cbd7b621b4495a828c2a455f55786b
sn <126475703+rsns-0@users.noreply.github.com>
2023-03-02 11:37:02 -0700

    added test cases for retry fn
    added tests

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,3 +1,12 @@
 import pytest
+from scripts.click_scripts import *
 
-
+def test_retry():
+    with pytest.raises(TriesExhausted) as excinfo:
+        top_left = (2243,223)
+        bot_right = (2542,474)
+        width = bot_right[0]-top_left[0]
+        height = bot_right[1]-top_left[1]
+        regionparam = (top_left[0], top_left[1], width, height)
+        quest_marker_coords = locate("quest_marker.png", region=regionparam)
+        print(quest_marker_coords) if quest_marker_coords else print('not found')

d195f8135603f67c691b8b1b3758ee891c804559
sn <126475703+rsns-0@users.noreply.github.com>
2023-03-06 04:38:09 -0700

    fixed locate functions to add region

--- scripts/click_scripts.py
+++ scripts/click_scripts.py
@@ -11,18 +11,15 @@ class NotFoundException(Exception):
 
 
 @retry(tries=3,delay=5,exceptions=NotFoundException)
-def locate(img_path, conf1=cfg.confidence, **kwargs):
-    if not kwargs.get('region'):
-        log1.info('Region was not passed in to locate function.')
-        raise NotImplementedError('Region was not passed in to locate function.')
-    if result := pa.locateCenterOnScreen(img_path, confidence=conf1, **kwargs):
+def locate(img_path, region, confidence=cfg.confidence, **kwargs):
+    if result := pa.locateCenterOnScreen(img_path, region=region, confidence=confidence, **kwargs):
         return result
     raise NotFoundException("Value was null.")
 
 
 @retry(tries=10,delay=60,exceptions=NotFoundException)
-def locate_long(img_path, conf1=cfg.confidence, **kwargs):
-    return locate(img_path, confidence=conf1, **kwargs)
+def locate_long(img_path, region, conf1=cfg.confidence, **kwargs):
+    return locate(img_path, confidence=conf1, region=region  **kwargs)
 
 
 def sleep_click(coordinates=None, sleep_time=1):

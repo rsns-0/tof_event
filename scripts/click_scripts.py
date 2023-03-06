24a81986a6cff54766927f45e4915edc4f51c1f6
sn <126475703+rsns-0@users.noreply.github.com>
2023-03-06 04:12:33 -0700

    add exception for missing region arg

--- scripts/click_scripts.py
+++ scripts/click_scripts.py
@@ -2,6 +2,9 @@ import pyautogui as pa
 from time import sleep
 from retry import retry
 from configs import click_config as cfg
+import logging
+logging.basicConfig(level=logging.INFO)
+log1 = logging.getLogger("click_scripts_logger")
 
 class NotFoundException(Exception):
     ...
@@ -9,6 +12,9 @@ class NotFoundException(Exception):
 
 @retry(tries=3,delay=5,exceptions=NotFoundException)
 def locate(img_path, conf1=cfg.confidence, **kwargs):
+    if not kwargs.get('region'):
+        log1.info('Region was not passed in to locate function.')
+        raise NotImplementedError('Region was not passed in to locate function.')
     if result := pa.locateCenterOnScreen(img_path, confidence=conf1, **kwargs):
         return result
     raise NotFoundException("Value was null.")

489cb99fe8077c1ab241e55184761bd18e380767
sn <126475703+rsns-0@users.noreply.github.com>
2023-03-06 04:08:02 -0700

    add region to locate

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from time import sleep
 from pathlib import Path
 import json
 import os
+import logging
+logging.basicConfig(level=logging.WARNING)
+logger = logging.getLogger('tof_event_log')
 
 os.getcwd()
 
@@ -18,14 +21,15 @@ def main():
     d = json_data
     while True:
         gift_img_path=img_path(d['gift_sword_area']['file_name'])
-        gift_coords = locate(gift_img_path)
+        gift_coords = locate(gift_img_path, region=d['gift_sword_area']['region'])
         sclick(*gift_coords)
         sclick(d['half_ani_button'])
         ff_img_path=img_path(d['frost_flame_img']['file_name'])
-        ff_coords = locate(ff_img_path)
+        ff_coords = locate(ff_img_path, region=d['ff_img_path']['region'])
         sclick(ff_coords)
         flame_icon_path = img_path(d['flame_icon_area']['file_name'])
-        locate_long(flame_icon_path)
+        flame_icon_coords = locate(flame_icon_path, region=d['flame_icon_path']['region'])
+        locate_long(flame_icon_coords)
         locate_long(gift_img_path)
 
 if __name__ == "__main__":

8e350560e756ce012e78e7a59bb9ea2a45513267
sn <126475703+rsns-0@users.noreply.github.com>
2023-03-06 07:50:07 -0700

    Added config files

--- configs/click_config.py
+++ configs/click_config.py
@@ -1,1 +1,14 @@
-confidence=0.50
+
+
+# Confidence is how confident the screen reader should when looking for the provided image. Lower values means it can detect false positives. Higher values can cause it to not detect anything.
+# Provide any number between 0.01 and 0.99.
+confidence=0.50 
+
+# How long (in seconds) to wait in between clicks. This can allow UI elements to load in so it doesn't click too early.
+sleep_click_default = 1
+
+
+
+
+# Not implemented yet. Do not touch.
+game_resolution=(1920,1080)
---
+++ configs/dev_config.py
@@ -0,0 +1,3 @@
+# Do not touch unless you know what you are doing or instructions said to change something here.
+
+data_file_name = "final_data_file.json"

51f87cff089b07ef944cf9164101ba2b0ed7829f
sn <126475703+rsns-0@users.noreply.github.com>
2023-03-06 07:48:41 -0700

    auto refactor

--- prototyping.py
+++ prototyping.py
@@ -1,5 +1,5 @@
 import pyautogui as pa
-from scripts.click_scripts import locate
+from scripts.click_scripts import valreturn_img
 
 def main():
     
@@ -8,7 +8,7 @@ def main():
     width = bot_right[0]-top_left[0]
     height = bot_right[1]-top_left[1]
     regionparam = (top_left[0], top_left[1], width, height)
-    quest_marker_coords = locate("quest_marker.png", region=regionparam)
+    quest_marker_coords = valreturn_img("quest_marker.png", region=regionparam)
     print(quest_marker_coords) if quest_marker_coords else print('not found')
     pa.moveTo(quest_marker_coords)
     pa.click()

e4b4cc09ec8e40607ee49a0f1b1f3be4ec705223
sn <126475703+rsns-0@users.noreply.github.com>
2023-03-06 23:31:53 -0700

    too many false positives at 0.5, changed to 0.7

--- configs/click_config.py
+++ configs/click_config.py
@@ -2,7 +2,7 @@
 
 # Confidence is how confident the screen reader should when looking for the provided image. Lower values means it can detect false positives. Higher values can cause it to not detect anything.
 # Provide any number between 0.01 and 0.99.
-confidence=0.50 
+confidence=0.7
 
 # How long (in seconds) to wait in between clicks. This can allow UI elements to load in so it doesn't click too early.
 sleep_click_default = 1

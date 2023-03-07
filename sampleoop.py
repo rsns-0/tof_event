9ba89408e99f6d11a4051e403cf110159306c7af
sn <126475703+rsns-0@users.noreply.github.com>
2023-03-07 03:35:24 -0700

    update setup.py

---
+++ sampleoop.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+import pyautogui
+
+@dataclass
+class Button:
+    coordinates:tuple[int,int]
+    
+    def click(self):
+        pyautogui.click(self.coordinates)
+
+@dataclass
+class HalfAnniversaryPage:
+    frost_and_flame:Button = Button((1234,1234))
+    fruit_dodging:Button = Button((1234,1234))
+    punch:Button = Button((1234,1234))
+    match:Button = Button((1234,1234))
+    
+    def start_punch(self):
+        self.punch.click()
+        self.match.click()
+
+
+half_anni_page:HalfAnniversaryPage = HalfAnniversaryPage()
+
+half_anni_page.start_punch()
